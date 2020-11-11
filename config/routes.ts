e2f91b65f2139003492a893c9f719f43c08735ab
陈林港 <Clare>
2020-11-11 12:17:36 +0800

    first commit

---
+++ config/routes.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    path: '/welcome',
+    name: '欢迎',
+    component: './Welcome',
+    hideInMenu: true,
+  },
+];
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Card, Alert, Typography } from 'antd';
+import styles from './Welcome.less';
+
+const CodePreview: React.FC<{}> = ({ children }) => (
+  <pre className={styles.pre}>
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
+);
+
+export default (): React.ReactNode => (
+  <>
+    <Card>
+      <Alert
+        message="Json Schema 风格的 CRUD。"
+        type="success"
+        showIcon
+        banner
+        style={{
+          margin: -12,
+          marginBottom: 24,
+        }}
+      />
+      <Typography.Text strong>
+        Json Schema CRUD{' '}
+        <a
+          href="https://www.npmjs.com/package/schema-page"
+          rel="noopener noreferrer"
+          target="__blank"
+        >
+          欢迎使用
+        </a>
+      </Typography.Text>
+      <CodePreview>yarn add schema-page</CodePreview>
+    </Card>
+  </>
+);
