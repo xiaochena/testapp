d0d3037d8cf88ff63a887c28eab078aae35cd554
Clare <clare@duomai.com>
2020-11-27 17:02:40 +0800

    [feat] Formily生命周期,未完成,只建了页面

---
+++ src/pages/Formily/03-生命周期/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Card } from 'antd';
+import { LifeCycleTypes, FormEffectHooks } from '@formily/antd'; //或者@formily/next
+
+const TheLifeCycle: React.FC = () => {
+  return <Card>TheLifeCycle</Card>;
+};
+export default TheLifeCycle;
