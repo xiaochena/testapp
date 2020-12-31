4f5e10b15da6345720c5e5e65efac86065aa3ae0
Clare <clare@duomai.com>
2020-12-31 18:23:22 +0800

    [feat] 工作echarts封装

--- src/pages/Echarts/00-工作中Echarts/index.tsx
+++ src/pages/Echarts/00-工作中Echarts/index.tsx
@@ -2,10 +2,11 @@ import React, { createRef, useEffect } from 'react';
 import { Card } from 'antd';
 import * as echarts from 'echarts';
 
+import chartConfig_01 from './01/chartConfig';
+
 const GetStartedECharts: React.FC = () => {
   const echartsRef1: any = createRef();
   const echartsRef2: any = createRef();
-  const echartsRef3: any = createRef();
 
   // echartsRef1 柱状图
   useEffect(() => {
@@ -77,137 +78,9 @@ const GetStartedECharts: React.FC = () => {
     ];
 
     if (echartsRef1.current) {
-      let intervalNum = 0;
       var myChart = echartsRef1.current && echarts.init(echartsRef1.current);
       // 绘制图表
-      myChart.setOption({
-        legend: {
-          data: ['增量', '总量'],
-        },
-        xAxis: {
-          type: 'category',
-          data: date,
-          axisTick: {
-            alignWithLabel: true,
-          },
-        },
-        tooltip: {
-          trigger: 'axis',
-        },
-        yAxis: [
-          {
-            name: '增量',
-            type: 'value',
-            max: (value: any) => (value.max * 6) / 5,
-            min: (value: any) => {
-              const interval = value.max / 5;
-              const min = value.min;
-              intervalNum = 0;
-              if (min < 0) {
-                const minAbc = Math.abs(min);
-                intervalNum = Math.ceil(minAbc / interval);
-              }
-              return -((intervalNum * value.max) / 5);
-            },
-            interval: Math.max(...Increment) / 5,
-            axisTick: {
-              alignWithLabel: true,
-            },
-          },
-          {
-            name: '总量',
-            type: 'value',
-            max: (value: any) => (value.max * 6) / 5,
-            min: (value: any) => (-intervalNum * value.max) / 5,
-            interval: Math.max(...Total) / 5,
-            axisTick: {
-              alignWithLabel: true,
-            },
-          },
-        ],
-        series: [
-          {
-            name: '增量',
-            data: Increment,
-            yAxisIndex: 0,
-            type: 'bar',
-            color: '#9ED9F0',
-            symbolSize: 10,
-            barMaxWidth: '30',
-            itemStyle: {
-              color(data: any) {
-                // 设置正负颜色值
-                return data.value >= 0
-                  ? {
-                      type: 'linear',
-                      x: 0,
-                      y: 0,
-                      x2: 0,
-                      y2: 1,
-                      colorStops: [
-                        {
-                          offset: 0,
-                          color: '#9ED9F0', // 0% 处的颜色
-                        },
-                        {
-                          offset: 1,
-                          color: 'rgba(255, 255, 255, 0.83)', // 100% 处的颜色
-                        },
-                      ],
-                      global: false, // 缺省为 false
-                    }
-                  : {
-                      type: 'linear',
-                      x: 0,
-                      y: 1,
-                      x2: 0,
-                      y2: 0,
-                      colorStops: [
-                        {
-                          offset: 0,
-                          color: '#ffad9a', // 0% 处的颜色
-                        },
-                        {
-                          offset: 1,
-                          color: 'rgba(255, 255, 255, 0.83)', // 100% 处的颜色
-                        },
-                      ],
-                      global: false, // 缺省为 false
-                    };
-              },
-            },
-          },
-          {
-            name: '总量',
-            data: Total,
-            yAxisIndex: 1,
-            type: 'line',
-            smooth: true,
-            color: '#817AF2',
-            symbolSize: 10,
-            areaStyle: {
-              color: {
-                type: 'linear',
-                x: 0,
-                y: 0,
-                x2: 0,
-                y2: 1,
-                colorStops: [
-                  {
-                    offset: 0,
-                    color: '#817AF2', // 0% 处的颜色
-                  },
-                  {
-                    offset: 1,
-                    color: 'rgba(255, 255, 255, 0.83)', // 100% 处的颜色
-                  },
-                ],
-                global: false, // 缺省为 false
-              },
-            },
-          },
-        ],
-      });
+      myChart.setOption(chartConfig_01(date, Increment, Total, 3));
     }
   }, []);
 
