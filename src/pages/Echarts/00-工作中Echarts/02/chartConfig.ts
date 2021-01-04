0b679d6f52ff8e6c5ea57a128ca589a0a04e7c4b
Clare <clare@duomai.com>
2021-01-04 14:10:55 +0800

    [feat] 品牌趋势折线图

---
+++ src/pages/Echarts/00-工作中Echarts/02/chartConfig.ts
@@ -0,0 +1,40 @@
+/**
+ * 图表配置
+ *
+ * @param {array} xAxisData x轴值
+ * @param {array} yIncrementData y轴数据
+ */
+
+interface YAxisData {
+  name: string;
+  data: any[];
+}
+
+export default (xAxisData: string[], yAxisData: YAxisData[]): object => {
+  return {
+    color: ['#817AF2', '#FF7FBA', '#8ED9F1', '#FFAD98', '#86A3FF'],
+    legend: {
+      data: yAxisData.map(item => item.name),
+      bottom: 0,
+      left: 'center',
+    },
+    tooltip: {
+      trigger: 'axis',
+    },
+    xAxis: {
+      type: 'category',
+      data: xAxisData,
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: yAxisData.map(item => {
+      return {
+        name: item.name,
+        data: item.data,
+        type: 'line',
+        symbolSize: 10,
+      };
+    }),
+  };
+};
--- src/pages/Echarts/00-工作中Echarts/index.tsx
+++ src/pages/Echarts/00-工作中Echarts/index.tsx
@@ -1,24 +1,31 @@
-import React, { createRef, useEffect } from 'react';
+import React, { createRef } from 'react';
 import { Card } from 'antd';
 
 import Chart01, { RefCurrent as RefCurrent01 } from './01';
+import Chart02, { RefCurrent as RefCurrent02 } from './02';
 
-import { DB_01 } from './DB';
+import { DB_01, DB_02 } from './DB';
 
 const GetStartedECharts: React.FC = () => {
-  const echartsRef1 = createRef<RefCurrent01>();
-  const echartsRef2: any = createRef();
+  const chartsRef1 = createRef<RefCurrent01>();
+  const chartsRef2: any = createRef<RefCurrent02>();
 
   return (
     <Card bodyStyle={{ display: 'flex' }} title="ECharts 入门示例">
       <Chart01
-        ref={echartsRef1}
         style={{ height: 300, flex: 1 }}
+        ref={chartsRef1}
         xData={DB_01.data}
         yIncrement={DB_01.Increment}
         yTotal={DB_01.Total}
       />
-      <div style={{ height: 300, flex: 1 }} ref={echartsRef2}></div>
+
+      <Chart02
+        style={{ height: 300, flex: 1 }}
+        ref={chartsRef2}
+        xData={DB_02.xAxisData}
+        yData={DB_02.yAxisData}
+      />
     </Card>
   );
 };
