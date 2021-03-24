d2d837421d10d2114db5d64ae86a70c656f36f5d
Clare <clare@duomai.com>
2021-03-24 16:16:30 +0800

    [style] 文案修改，以及不知名bug菜单无法收起的修复测试

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -15,7 +15,7 @@ export default (): React.ReactNode => (
   <>
     <Card>
       <Alert
-        message="谁便写写Demo"
+        message="随便写写Demo"
         type="success"
         showIcon
         banner
@@ -25,7 +25,7 @@ export default (): React.ReactNode => (
         }}
       />
       <Typography.Text strong>
-        谁便写写Demo
+        随便写写Demo
         <a
           href="https://www.npmjs.com/package/schema-page"
           rel="noopener noreferrer"
