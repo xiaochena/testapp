b3ad15207009ada1fac3aa4b27ec6caa13e785e7
Xiaochena <chen.lingang@qq.com>
2021-07-20 20:23:40 +0800

    [style] 布局

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -12,30 +12,28 @@ const CodePreview: React.FC<{}> = ({ children }) => (
 );
 
 export default (): React.ReactNode => (
-  <>
-    <Card>
-      <Alert
-        message="随便写写Demo"
-        type="success"
-        showIcon
-        banner
-        style={{
-          margin: -12,
-          marginBottom: 24,
-        }}
-      />
-      <Typography.Text strong>
-        随便写写Demo
-        <a
-          href="https://www.npmjs.com/package/schema-page"
-          rel="noopener noreferrer"
-          target="__blank"
-        >
-          欢迎使用
-        </a>
-      </Typography.Text>
-      <CodePreview>yarn</CodePreview>
-      <CodePreview>yarn start</CodePreview>
-    </Card>
-  </>
+  <Card>
+    <Alert
+      message="随便写写Demo"
+      type="success"
+      showIcon
+      banner
+      style={{
+        margin: -12,
+        marginBottom: 24,
+      }}
+    />
+    <Typography.Text strong>
+      随便写写Demo
+      <a
+        href="https://www.npmjs.com/package/schema-page"
+        rel="noopener noreferrer"
+        target="__blank"
+      >
+        欢迎使用
+      </a>
+    </Typography.Text>
+    <CodePreview>yarn</CodePreview>
+    <CodePreview>yarn start</CodePreview>
+  </Card>
 );
